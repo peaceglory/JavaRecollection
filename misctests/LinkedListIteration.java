1b97977c4e978eb36ab3f0b2a7fc54f9a4b19191
mman <miroslav.manchev@gmail.com>
2016-10-23 19:32:46 +0300

    Add some misc tests.

---
+++ misctests/LinkedListIteration.java
@@ -0,0 +1,29 @@
+package misctests;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
+/**
+ * Created by mman on 22.10.16.
+ */
+public class LinkedListIteration {
+    public static void main(String[] args) {
+        final List<Integer> lst = new LinkedList<>();
+        for ( int i = 0; i < 1000000; ++i )
+            lst.add( i );
+        long sum = 0;
+        long then = System.currentTimeMillis();
+        ListIterator<Integer> iter = lst.listIterator();
+        int i = 0;
+        while (iter.hasNext()) {
+            sum += iter.next();
+            lst.get(i++);
+        }
+//        for ( int i = 0; i < 1_000_000; ++i )
+//            sum += lst.get( i );
+        long now = System.currentTimeMillis();
+
+        System.out.println("IT all took: " + (now - then)/1000 + " sec. Sum: " + sum);
+    }
+}
---
+++ misctests/LinkedListTest.java
@@ -0,0 +1,31 @@
+package misctests;
+
+import java.util.LinkedList;
+
+/**
+ * Created by mman on 22.10.16.
+ */
+public class LinkedListTest {
+
+    public static void main(String[] args) {
+        final int PREFILL_COUNT = 100_000;
+        final int LOOP_COUNT = 300_000_000;
+        final LinkedList<Integer> lst = new LinkedList<Integer>();
+        final Integer val = 1;
+
+        for ( int i = 0; i < PREFILL_COUNT; ++i )
+            lst.add( 35 );
+
+        long then = System.currentTimeMillis();
+        for ( int i = 0; i < LOOP_COUNT; ++i )
+        {
+            for ( int j = 0; j < 5; ++j )
+                lst.addFirst( val );
+            for ( int j = 0; j < 5; ++j )
+                lst.removeLast();
+        }
+        long now = System.currentTimeMillis();
+
+        System.out.println("IT all took: " + (now - then)/1000 + " sec.");
+    }
+}
