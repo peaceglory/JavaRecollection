a98c8cab4c59cb0e6801c4a1c00a7ea24335c412
mman <miroslav.manchev@gmail.com>
2016-11-04 12:44:41 +0200

    Moving to new packet.

---
+++ tests/datastructures/CustomDoubleLinkedListTest.groovy
@@ -0,0 +1,53 @@
+package tests.datastructures
+
+import sources.datastructures.CustomDoubleLinkedList
+/**
+ * Created by mman on 23.10.16.
+ */
+class CustomDoubleLinkedListTest extends GroovyTestCase {
+
+    void testAdd() {
+        CustomDoubleLinkedList list = new CustomDoubleLinkedList();
+
+        list.add("One");
+        list.add("Two");
+        list.add("Three");
+        assert list.size() == 3;
+    }
+
+    void testRemoveByIndex() {
+        CustomDoubleLinkedList list = new CustomDoubleLinkedList();
+        list.add("One");
+        list.add("Two");
+        list.add("Three");
+        list.add("Four");
+        list.add("Five");
+
+        list.remove(3);
+        assert list.size() == 4 && list.get(3) == "Five";
+
+        list.remove(3);
+        assert list.size() == 3 && list.get(2) == "Three";
+
+        list.remove(0);
+        assert list.size() == 2 && list.get(0) == "Two";
+
+        list.remove(1);
+        assert list.size() == 1 && list.get(0) == "Two";
+
+        boolean excCaught = false;
+        try {
+            list.remove(10);
+        } catch (IndexOutOfBoundsException iob) {
+            excCaught = true;
+        }
+        assert excCaught;
+
+        list.remove(0);
+        assert list.size() == 0;
+    }
+
+    void testRemoveByValue() {
+
+    }
+}
---
+++ tests/datastructures/CustomDynamicStackTest.groovy
@@ -0,0 +1,24 @@
+package tests.datastructures
+
+import sources.datastructures.CustomDynamicStack
+
+/**
+ * Created by mman on 24.10.16.
+ */
+class CustomDynamicStackTest extends GroovyTestCase {
+
+    void testPush() {
+        CustomDynamicStack stack = new CustomDynamicStack();
+        assert stack.pop() == null && stack.size() == 0;
+
+        stack.push("A");
+        assert stack.size() == 1 && stack.pop() == "A" && stack.size() == 0;
+
+        stack.push("A").push("B").push("C");
+        assert stack.size() == 3 && stack.pop() == "C" && stack.pop() == "B" && stack.pop() == "A";
+    }
+
+    void testPop() {
+
+    }
+}
