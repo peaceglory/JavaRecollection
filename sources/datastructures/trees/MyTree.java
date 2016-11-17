0db978929c9ccb6350a882946290cc0f6ac3b27e
mman <miroslav.manchev@gmail.com>
2016-11-17 12:07:43 +0200

    Beginning MyTree.java

---
+++ sources/datastructures/trees/MyTree.java
@@ -0,0 +1,28 @@
+package sources.datastructures.trees;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by mman on 16.11.16.
+ */
+public class MyTree<T> {
+    private Node<T> root = null;
+
+    public MyTree(T rootValue) {
+        root = new Node<T>(rootValue);
+    }
+
+    public MyTree(T rootValue, T... childrenValue) {
+
+    }
+
+    private static class Node<T> {
+        private T value = null;
+        private List<Node<T>> children = new ArrayList<>();
+
+        private Node(T value) {
+            this.value = value;
+        }
+    }
+}
