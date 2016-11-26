fd83c22993ee675061e9ac6e40fde0859339c248
mman <miroslav.manchev@gmail.com>
2016-11-26 17:09:01 +0200

    Remake MyBinarySearchTree::search() to be iterative.

--- sources/datastructures/trees/MyBinarySearchTree.java
+++ sources/datastructures/trees/MyBinarySearchTree.java
@@ -38,17 +38,16 @@ public class MyBinarySearchTree<T extends Comparable> extends MyBinaryTree<T> {
 
     @Override
     public IMyTree search(T value) {
-        if (this.value.compareTo(value) == 0) {
-            return this;
-        }
-        if (this.value.compareTo(value) > 0) {
-            if (left != null) {
-                return left.search(value);
+        MyBinaryTree current = this;
+        while (current != null) {
+            if (current.value.compareTo(value) == 0) {
+                return current;
             }
-            return null; // Not found.
-        }
-        if (right != null) {
-            return right.search(value);
+            if (current.value.compareTo(value) > 0) {
+                current = current.left;
+            } else {
+                current = current.right;
+            }
         }
         return null;
     }
