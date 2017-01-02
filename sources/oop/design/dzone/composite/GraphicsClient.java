78075b3a3f07c976837a6c2f52a6532b26a322a2
mman <miroslav.manchev@gmail.com>
2017-01-02 14:40:10 +0200

    Again forgot client code.

---
+++ sources/oop/design/dzone/composite/GraphicsClient.java
@@ -0,0 +1,12 @@
+package sources.oop.design.dzone.composite;
+
+//Client.
+public class GraphicsClient {
+
+    /**
+     * Given a graphics context, client can just call paint, without worrying if this is a composite or leaf
+     **/
+    public void paint(Graphic g) {
+        g.paint();
+    }
+}
