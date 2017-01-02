97c8126666b55cf2ed4346fe2b4e0f03d85647a0
mman <miroslav.manchev@gmail.com>
2017-01-02 14:37:16 +0200

    Add Composite examples.

---
+++ sources/oop/design/dzone/composite/SimpleGraphic.java
@@ -0,0 +1,27 @@
+package sources.oop.design.dzone.composite;
+
+//Leaf
+public class SimpleGraphic implements Graphic {
+
+    public void paint() {
+        //run the paint operation
+    }
+
+    /** Because we have no children, these operations will do nothing  **/
+    public void add(Graphic g) {
+        //unsupported operation
+    }
+
+    public void remove(Graphic g) {
+        //unsupported operation
+    }
+
+    public Graphic get(int index) {
+        //unsupported operation
+        return null;
+    }
+}
+
+
+
+
