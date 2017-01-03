e962c3fe229c1fe00fb807a4746fcdcd50678c3b
mman <miroslav.manchev@gmail.com>
2017-01-03 12:01:15 +0200

    Add Bridge examples from DZone.

---
+++ sources/oop/design/dzone/bridge/ConcreteRemote.java
@@ -0,0 +1,16 @@
+package sources.oop.design.dzone.bridge;
+
+//Refined abstraction
+public class ConcreteRemote extends RemoteControl {
+    private int currentChannel;
+
+    public void nextChannel() {
+        currentChannel++;
+        setChannel(currentChannel);
+    }
+
+    public void prevChannel() {
+        currentChannel--;
+        setChannel(currentChannel);
+    }
+}
