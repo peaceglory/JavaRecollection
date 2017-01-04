edcfe733eea36434ed6d1f3a38165bdc52891ecb
mman <miroslav.manchev@gmail.com>
2017-01-04 12:56:14 +0200

    Add Observer examples.

---
+++ sources/oop/design/com/javacodegeeks/patterns/observerpattern/Subject.java
@@ -0,0 +1,12 @@
+package sources.oop.design.com.javacodegeeks.patterns.observerpattern;
+
+public interface Subject {
+	
+	public void subscribeObserver(Observer observer);
+
+	public void unSubscribeObserver(Observer observer);
+
+	public void notifyObservers();
+
+	public String subjectDetails();
+}
