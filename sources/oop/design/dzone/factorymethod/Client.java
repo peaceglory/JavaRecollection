d06f3e5b056054d975dc769bbf13bbf597e52d89
mman <miroslav.manchev@gmail.com>
2017-01-11 18:17:42 +0200

    Add javadoc for Factory Method examples.

--- sources/oop/design/dzone/factorymethod/Client.java
+++ sources/oop/design/dzone/factorymethod/Client.java
@@ -1,5 +1,28 @@
 package sources.oop.design.dzone.factorymethod;
 
+
+/**
+ * <p>
+ * <b>Intent:</b><br />
+ * Relives clients of a service from knowing which class they need to instantiate.
+ * </p>
+ *
+ * <p>
+ * <b>Ups:</b><br />
+ * The client needs to be aware of only the service interface but remain agnostic which of the extending classes
+ * would do the job.
+ * </p>
+ *
+ * <p>
+ * <b>Downs:</b><br />
+ * If it is not actually needed it brings unnecessary complexity.
+ * </p>
+ *
+ * <p>
+ * <b>Examples:</b><br />
+ * Iterators in JDK, Different loggers, Different XML parsers.
+ * </p>
+ */
 public class Client {
 
     private void someMethodThatLogs(AbstractLoggerCreator logCreator) {
