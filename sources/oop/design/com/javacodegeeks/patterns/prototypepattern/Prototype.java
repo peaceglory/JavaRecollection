d83fc81f026fc062f0331cf5891800ecf1699f7d
mman <miroslav.manchev@gmail.com>
2017-01-13 12:45:08 +0200

    Add javacodegeeks Prototype example.

---
+++ sources/oop/design/com/javacodegeeks/patterns/prototypepattern/Prototype.java
@@ -0,0 +1,7 @@
+package sources.oop.design.com.javacodegeeks.patterns.prototypepattern;
+
+public interface Prototype extends Cloneable {
+	
+	public AccessControl clone() throws CloneNotSupportedException;
+
+}
