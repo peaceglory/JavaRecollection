5171cb1ca5451acecd9d23ba13575f6847c1abc2
mman <miroslav.manchev@gmail.com>
2017-01-14 18:11:47 +0200

    Add State examples.

---
+++ sources/oop/design/com/javacodegeeks/patterns/statepattern/RoboticState.java
@@ -0,0 +1,9 @@
+package sources.oop.design.com.javacodegeeks.patterns.statepattern;
+
+public interface RoboticState {
+	
+	public void walk();
+	public void cook();
+	public void off();
+
+}
---
+++ sources/oop/design/dzone/state/PlayingState.java
@@ -0,0 +1,8 @@
+package sources.oop.design.dzone.state;
+
+public class PlayingState implements State {
+
+    public void pressPlay(MP3PlayerContext context) {
+        context.setState(new StandbyState());
+    }
+}
