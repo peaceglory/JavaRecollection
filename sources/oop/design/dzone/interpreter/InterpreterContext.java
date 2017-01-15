f9eb4c510ad3b697c83cccc65a9d970f55ef4756
mman <miroslav.manchev@gmail.com>
2017-01-15 18:05:19 +0200

    Add Interpreter examples.

---
+++ sources/oop/design/dzone/interpreter/InterpreterContext.java
@@ -0,0 +1,21 @@
+package sources.oop.design.dzone.interpreter;
+
+import java.util.ArrayList;
+
+//Context
+public class InterpreterContext {
+    //assume web service is setup
+    private AmazonWebService webService;
+
+    public InterpreterContext(String endpoint) {
+        //create the web service.
+    }
+
+    public ArrayList<Movie> getAllMovies() {
+        return webService.getAllMovies();
+    }
+
+    public ArrayList<Book> getAllBooks() {
+        return webService.getAllBooks();
+    }
+}
