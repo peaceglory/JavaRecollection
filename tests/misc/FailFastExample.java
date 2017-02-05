0e25c616d4d9019f4abce88f2bc70278906513c1
mman <miroslav.manchev@gmail.com>
2017-02-05 18:02:12 +0200

    Add some basic tests with collections.

--- tests/misc/FailFastExample.java
+++ tests/misc/FailFastExample.java
@@ -5,25 +5,21 @@ import java.util.Iterator;
 import java.util.Map;
 
 /* http://javahungry.blogspot.com/2014/04/fail-fast-iterator-vs-fail-safe-iterator-difference-with-example-in-java.html */
-public class FailFastExample
-{
+public class FailFastExample {
 
-
-    public static void main(String[] args)
-    {
-        Map<String,String> premiumPhone = new HashMap<String,String>();
+    public static void main(String[] args) {
+        Map<String, String> premiumPhone = new HashMap<String, String>();
         premiumPhone.put("Apple", "iPhone");
         premiumPhone.put("HTC", "HTC one");
-        premiumPhone.put("Samsung","S5");
+        premiumPhone.put("Samsung", "S5");
 
         Iterator iterator = premiumPhone.keySet().iterator();
 
-        while (iterator.hasNext())
-        {
+        while (iterator.hasNext()) {
             System.out.println(premiumPhone.get(iterator.next()));
-//            iterator.remove();
-            premiumPhone.remove("Apple");
-//            premiumPhone.put("Sony", "Xperia Z");
+//            iterator.remove(); // No problem
+//            premiumPhone.remove("Apple"); // Fails fast.
+            premiumPhone.put("Sony", "Xperia Z"); // Fails fast.
         }
 
     }
