79e6b501eee5f8d3a34554e9560571cf7f1a398d
Miroslav Manchev <miroslav.manchev@accelya.com>
2020-12-23 22:24:21 +0200

    .

--- src/main/java/postjava6/java8/streams/StreamTask1.java
+++ src/main/java/postjava6/java8/streams/StreamTask1.java
@@ -1,13 +1,13 @@
 package postjava6.java8.streams;
 
-import com.sun.deploy.ref.AppModel;
-
 import java.util.Arrays;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * [[Pesho, 4], [Yana, 6], [Miro, 5], [Eli, 6], [Miro, 6], [Eli, 5]]
@@ -15,8 +15,17 @@ import org.junit.jupiter.api.Test;
  */
 public class StreamTask1 {
 
+    public Map<String, Double> findAverage(String[][] matrix) {
+        return Arrays.stream(matrix).collect(
+                Collectors.groupingBy(
+                        person -> person[0],
+                        Collectors.mapping(
+                                arr -> Double.valueOf(arr[1]),
+                                Collectors.averagingDouble(value -> value))));
+    }
+
     @Test
-    void findAverage() {
+    void test_findAverage() {
         final String[][] matrix = {
                 {"Pesho", "4"},
                 {"Yana", "6"},
@@ -28,24 +37,15 @@ public class StreamTask1 {
                 {"Gogo", "6"}
         };
 
-        /*
-        * 1. Create a Map<String, List<String[]>>
-        * 2. Process each array in the stream
-        *   - map.put(arr[0], arr)
-        *   - Map is now constructed
-        * 3. Process each entry in the map
-        *   - process each item in the value list
-        *       - convert the item to number
-        *   - reduce the list to an average
-        * 4. A Map<String, Double> is produced as a final result.
-        */
-        final Map<String, Double> collect = Arrays.stream(matrix).collect(
-                Collectors.groupingBy(
-                        person -> person[0],
-                        Collectors.mapping(
-                                arr -> Integer.valueOf(arr[1]),
-                                Collectors.averagingDouble(value -> value))));
+        final Map<String, Double> collect = findAverage(matrix);
 
-        System.out.println(collect);
+        final Map<String, Double> expected = new HashMap<>(collect.size());
+        expected.put("Gogo", 4.0);
+        expected.put("Yana", 6.0);
+        expected.put("Eli", 5.5);
+        expected.put("Pesho", 4.0);
+        expected.put("Miro", 5.5);
+
+        assertEquals(expected, collect);
     }
 }
